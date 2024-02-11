fbccdb1c5087d46a1f2ffd5be4e467926b2f740f
anthony allard <allard.anthony@hotmail.fr>
2024-02-11 23:10:54 +0100

    page products

--- ecommerce2/client/src/components/FeaturedProducts/FeatureProducts.jsx
+++ ecommerce2/client/src/components/FeaturedProducts/FeatureProducts.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import "./FeatureProducts.scss"
-import { Key } from '@mui/icons-material'
-import { Card } from '@mui/material'
+// import React from 'react'
+// import "./FeatureProducts.scss"
+// import { Key } from '@mui/icons-material'
+// import { Card } from '@mui/material'
 
-const FeatureProducts = () => {
-  return (
-    <><div className='top'>
-          <h1>{type} products</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, aliquid? Fugiat, doloremque iusto perspiciatis distinctio, harum consequuntur molestias accusantium iure amet modi reprehenderit beatae voluptates ex temporibus cupiditate minus nisi.</p>
-      </div><div className='bottom'>
-              {data.map(items => {
-                  <Card item={item} key={item.id} />
-              })}
-          </div></>
-  )
-}
+// const FeatureProducts = () => {
+//   return (
+//     <><div className='top'>
+//           <h1>{type} products</h1>
+//           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, aliquid? Fugiat, doloremque iusto perspiciatis distinctio, harum consequuntur molestias accusantium iure amet modi reprehenderit beatae voluptates ex temporibus cupiditate minus nisi.</p>
+//       </div><div className='bottom'>
+//               {data.map(items => {
+//                   <Card item={item} key={item.id} />
+//               })}
+//           </div></>
+//   )
+// }
 
 
 
-export default FeatureProducts
+// export default FeatureProducts
--- ecommerce2/client/src/pages/Home/Home.jsx
+++ ecommerce2/client/src/pages/Home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
-import FeatureProducts from '../../components/FeaturedProducts/FeatureProducts.jsx'
+// import FeatureProducts from '../../components/FeaturedProducts/FeatureProducts.jsx'
+import Categories from '../../components/Categories/Categories.jsx'
+import Contact from '../../components/Contact/Contact.jsx'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
-      <FeatureProducts type="featured"/>
-      <FeatureProducts type="trending"/>
+      {/* <FeatureProducts type="featured"/> */}
+      <Categories/>
+      {/* <FeatureProducts type="trending"/> */}
+      <Contact/>
     </div>
   )
 }
