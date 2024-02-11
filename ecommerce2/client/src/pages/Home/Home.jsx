bcb168212aea0441cb6acfe8d7f88e86231997ea
anthony allard <allard.anthony@hotmail.fr>
2024-02-11 01:55:27 +0100

    featured

--- ecommerce2/client/src/pages/Home/Home.jsx
+++ ecommerce2/client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
+import FeatureProducts from '../../components/FeaturedProducts/FeatureProducts.jsx'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
+      <FeatureProducts type="featured"/>
+      <FeatureProducts type="trending"/>
     </div>
   )
 }
